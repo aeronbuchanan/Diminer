0db9dbd7f58323b702f0378772a9a15106ba1627
Aeron Buchanan <user@example.net>
2017-03-07 17:26:46 +1100

    Moved unused debug functions to separate file

---
+++ debug.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright, 2013, Aeron Buchanan
+ *
+ * This file is part of Diminer, a digital inpainting resource.
+ *
+ * Diminer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Diminer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Diminer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+//! Useful debug typename identifiers
+template<typename T> std::string identifyType(T const &) { return "unknown"; }
+template<> std::string identifyType(int const &) { return "int"; }
+template<> std::string identifyType(uint const &) { return "uint"; }
+template<> std::string identifyType(char const &) { return "char"; }
+template<> std::string identifyType(uchar const &) { return "uchar"; }
+template<> std::string identifyType(float const &) { return "float"; }
+template<> std::string identifyType(double const &) { return "double"; }
+
